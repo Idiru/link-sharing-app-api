91d286fda8f80ee92a45f3b826db1a4cbb3701be
Nawelchafi <chafinawel@gmail.com>
2024-06-02 11:10:22 +0200

    created user model

---
+++ link-sharing-api/models/User.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required.']
+    }
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`    
+    timestamps: true
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
