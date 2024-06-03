552207fc6610a702a4e64741a5ca95419db1260e
Idir FOUQUET <fouquetidir@gmail.com>
2024-06-03 19:56:44 +0200

    /auth/signup done

--- link-sharing-api/routes/auth.routes.js
+++ link-sharing-api/routes/auth.routes.js
@@ -7,6 +7,74 @@ const { isAuthenticated } = require("./../middleware/jwt.middleware")
 
 const router = express.Router();
 const saltRounds = 10;
+let responseSent = false; //Allow us to stop  the code in different promesses if an error is sent
+
+
+// POST /auth/signup  - Creates a new user in the database
+router.post('/signup', (req, res, next) => {
+    const { email, password, firstName, lastName } = req.body;
+   
+    // Check if the email, password, first name and last name are provided as an empty string 
+    if (email === '' || password === '' || firstName === '' || lastName === '') {
+      res.status(400).json({ message: "Provide email, password, first name and last name" });
+      responseSent = true; 
+      return;
+    }
+   
+    // Use regex to validate the email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    if (!emailRegex.test(email)) {
+      res.status(400).json({ message: 'Provide a valid email address.' });
+      responseSent = true; 
+      return;
+    }
+    
+    // Use regex to validate the password format
+    const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!passwordRegex.test(password)) {
+      res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
+      responseSent = true; 
+      return;
+    }
+   
+   
+    // Check the users collection if a user with the same email already exists
+    User.findOne({ email })
+      .then((foundUser) => {
+        // If the user with the same email already exists, send an error response
+        if (foundUser) {
+          res.status(400).json({ message: "User already exists." });
+          responseSent = true; 
+          return;
+        }
+   
+        // If the email is unique, proceed to hash the password
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+   
+        // Create a new user in the database
+        // We return a pending promise, which allows us to chain another `then` 
+        return User.create({ email, password: hashedPassword, firstName, lastName });
+      })
+      .then((createdUser) => {
+        // Deconstruct the newly created user object to omit the password
+        // We should never expose passwords publicly
+        if (responseSent) return; 
+        const { email, firstName, lastName, _id } = createdUser;
+      
+        // Create a new object that doesn't expose the password
+        const user = { email, firstName, lastName, _id };
+   
+        // Send a json response containing the user object
+        res.status(201).json({ user: user });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "Internal Server Error" })
+      });
+  });
+
+
 // POST  /auth / login - Verifies email and password and returns a JWT
 router.post('/login', (req, res, next) => {
     const { email, password } = req.body;
