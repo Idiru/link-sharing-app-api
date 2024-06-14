4299847d7148cf5adf46a78ab17f7e8a8e0aa4bf
Idir FOUQUET <fouquetidir@gmail.com>
2024-06-15 00:04:39 +0200

    Fixes Mogodb connexion

--- db/index.js
+++ db/index.js
@@ -11,7 +11,7 @@ const MONGO_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/link-sharing-api";
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => console.log(`Connected the Database: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
 
