8dbb13fb2c1f505a43dbadc65c7a24e958665e4d
Nawelchafi <chafinawel@gmail.com>
2024-06-27 21:18:01 +0200

    added userName fetch route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -22,5 +22,23 @@ router.get("/username", isAuthenticated, (req, res) => {
     })
     .catch(error => res.status(500).json(error));
 });
+// GET /users/username/:username - Get user by username
+router.get("/users/username/:username", isAuthenticated, (req, res) => {
+  const { username } = req.params;
+
+  if (!username) {
+    return res.status(400).json({ message: "Username is required" });
+  }
+
+  User.findOne({ userName: username })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json({ user });
+    })
+    .catch((error) => res.status(500).json(error));
+});
+
 
 module.exports = router;
