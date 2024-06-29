e3572ec72eb76778f288abc9b0673a5f5fe7cfc1
Idir FOUQUET <fouquetidir@gmail.com>
2024-06-29 13:01:21 +0200

    Comments and readme content features

--- README.md
+++ README.md
@@ -1,23 +1,74 @@
 # link-sharing-app-api
 
+## Global
+
+### Environment variables
+
+- `PORT`: To define the port you want to use.
+- `ORIGIN`: To define the front url (to avoid CORS issues)
+- `TOKEN_SECRET`: To uncode the JWT tokens
+- `MONGODB_URI`: To connect to your DB
+
 ## Authentification
-The application allow you to have and account. 
-The authentification is token-based (JWT) and is composed of 3 routes: 
+
+The application allow you to have and account.
+The authentification is token-based (JWT) and is composed of 3 routes:
+
 1. POST /auth/signup to create an account
 2. POST /auth/login to login
 3. GET /auth/verify to verify you're logged
 
 ### Signup
+
 To signup, you have to provide these data:
+
 - email
 - password
 - firstName
 - lastName
 
 The following checks are done:
+
 - None of them are empties
 - The email format is valid
 - The password guidelines are followd (at least 6 characters and contain at least one number, one lowercase and one uppercase letter)
 - Dupplicate users (already existing email)
 
-If everything is OK, your data are stored with a hashed version of your password. 
+If everything is OK, your data are stored with a hashed version of your password.
+
+### Content
+
+### Overview
+
+The content only allows the user to create a new link block for the moment.
+The endpoints start by `/content`.
+
+### Core functionalities
+
+- <u>**Add content:**</u>
+  To create a new link, you have to provide these data:
+
+  - block
+  - platform
+  - url
+  - title
+
+  Here is the endpoint: `/create`
+
+- <u>**Get content:**</u>
+  You can access to a specific content data using a GET request if you have its id.
+
+  Here is the endpoint: `/:contentId`
+
+- <u>**Update content:**</u>
+  Your can update all the data of an existing content (fields available above) using a POST request if you have its id.
+
+  The endpoint is the same than the "Get content" above.
+
+- <u>**Delete content:**</u>
+  You can delete to a specific content data using a DELETE request if you have its id.
+
+  The endpoint is the same than the "Get content" above.
+
+In case the id is needed, we extract it from the payload of the request. 
+The content is linked to a user, a dedicated column exists inside the user table of the DB. 
--- routes/content.routes.js
+++ routes/content.routes.js
@@ -4,115 +4,125 @@ const Content = require("../models/Content.model");
 const User = require("../models/User.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
-
 // POST  /create
 router.post("/create", isAuthenticated, (req, res) => {
-    const { block, platform, url, title } = req.body;
-    const userId = req.payload._id; // Extract user ID from payload
-    console.log(userId)
-    // Create a new entry inside the content collection
-    Content.create({ block, platform, url, title, user: userId }) // Include user ID
-        .then((newContent) => {
-
-            User.findByIdAndUpdate(userId,
-                { $push: { content: newContent._id } }, // Use $push to add new content ID
-                { new: true, safe: true, upsert: true }) // Ensure creation if not exists, and return updated document
-                .then(user => {
-                    console.log('Updated user with new content:', user);
-                    res.status(201).json(newContent);
-                })
-                .catch(err => {
-                    console.error('Error updating user:', err);
-                    res.status(500).json({ message: "Failed to update user with new content" });
-                });
-
+  const { block, platform, url, title } = req.body; 
+  const userId = req.payload._id; // Extract user ID from payload
+  // Create a new entry inside the content collection
+  Content.create({ block, platform, url, title, user: userId }) // We add the content data and we include the user ID
+    .then((newContent) => {
+      User.findByIdAndUpdate( //We need to populate the content inside the its dedicated field of the User DB collection
+        userId,
+        { $push: { content: newContent._id } }, // Use $push to add new content ID
+        { new: true, safe: true, upsert: true }
+      ) // Ensure creation if not exists, and return updated document
+        .then((user) => {
+          console.log("Updated user with new content:", user);
+          res.status(201).json(newContent);
         })
-        .catch((err) =>
-            res.status(500).json({ message: "Failed to create content" })
-        );
+        .catch((err) => {
+          console.error("Error updating user:", err);
+          res
+            .status(500)
+            .json({ message: "Failed to update user with new content" });
+        });
+    })
+    .catch((err) =>
+      res.status(500).json({ message: "Failed to create content" })
+    );
 });
 
-// GET /:contentId - Get content 
+// GET /:contentId - Get content
 router.get("/:contentId", isAuthenticated, (req, res) => {
-    const contentId = req.params.contentId;
+  const contentId = req.params.contentId; //Base on the content id, we return the data of this specific content
 
-    if (!mongoose.Types.ObjectId.isValid(contentId)) {
-        res.status(400).json({ message: `Specified id is not valid` });
-        return;
-    }
+  if (!mongoose.Types.ObjectId.isValid(contentId)) {
+    res.status(400).json({ message: `Specified id is not valid` });
+    return;
+  }
 
-    Content.findById(contentId)
-        .then((content) => {
-            if (!content) {
-                return res.status(404).json({ message: "Content not found" });
-            }
-            res.json(content);
-        })
-        .catch(error => res.status(500).json({ message: "Error getting the content" }));
+  Content.findById(contentId)
+    .then((content) => {
+      if (!content) {
+        return res.status(404).json({ message: "Content not found" });
+      }
+      res.json(content);
+    })
+    .catch((error) =>
+      res.status(500).json({ message: "Error getting the content" })
+    );
 });
-
 
 // PUT  /:contentID
 router.put("/:contentId", isAuthenticated, (req, res) => {
-    const contentId = req.params.contentId;
-    const update = req.body;
+  const contentId = req.params.contentId;
+  const update = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(contentId)) {
-        res.status(400).json({ message: `Specified id is not valid` });
-        return;
-    }
+  if (!mongoose.Types.ObjectId.isValid(contentId)) {
+    res.status(400).json({ message: `Specified id is not valid` });
+    return;
+  }
 
-    Content.findByIdAndUpdate(contentId, update, { new: true })
-        .then((updatedContent) => {
-            if (!updatedContent) {
-                return res.status(404).json({ message: "Content not found" });
-            }
-            res.json(updatedContent);
-        })
-        .catch(error => res.status(500).json({ message: "Error updating the content" }));
+  Content.findByIdAndUpdate(contentId, update, { new: true })
+    .then((updatedContent) => {
+      if (!updatedContent) {
+        return res.status(404).json({ message: "Content not found" });
+      }
+      res.json(updatedContent);
+    })
+    .catch((error) =>
+      res.status(500).json({ message: "Error updating the content" })
+    );
 });
 
-
 // Delete  /:contentID
-router.delete("/:contentId", isAuthenticated, (req, res) => {
-    const contentId = req.params.contentId;
+router.delete("/:contentId", isAuthenticated, async (req, res) => {
+  const contentId = req.params.contentId;
 
-    if (!mongoose.Types.ObjectId.isValid(contentId)) {
-        res.status(400).json({ message: `Specified id is not valid` });
-        return;
-    }
+  if (!mongoose.Types.ObjectId.isValid(contentId)) {
+    res.status(400).json({ message: `Specified id is not valid` });
+    return;
+  }
 
-    Content.findByIdAndDelete(contentId)
-        .then((deletedContent) => {
-            if (!deletedContent) {
-                return res.status(404).json({ message: "Content not found" });
-            }
-            res.json({ message: "Content deleted successfully" });
-        })
-        .catch(error => res.status(500).json({ message: "Error deleting the content" }));
+  Content.findByIdAndDelete(contentId)
+    .then((deletedContent) => { //We delete the user form the Content collection
+      if (!deletedContent) {
+        return res.status(404).json({ message: "Content not found" });
+      }
+      return User.updateMany( //The content reference also has to be deleted from the User collection
+        { content: { $in: [contentId] } },
+        { $pull: { content: contentId } }
+      ).then(() => {
+        res.json({ message: "Content deleted successfully" });
+      });
+    })
+
+    .catch((error) =>
+      res.status(500).json({ message: "Error deleting the content" })
+    );
 });
 
 // GET /content/:userId - Get all content created by a specific user
 router.get("/users/:userId", isAuthenticated, (req, res) => {
-    const userId = req.params.userId;
-    console.log(userId)
-    Content.find({ user: userId })
-        .populate('user')
-        .then((userContents) => {
-            res.json(userContents);
-        })
-        .catch((err) =>
-            res.status(500).json({ message: "Failed to retrieve user content" })
-        );
+  const userId = req.params.userId;
+  console.log(userId);
+  Content.find({ user: userId })
+    .populate("user")
+    .then((userContents) => {
+      res.json(userContents);
+    })
+    .catch((err) =>
+      res.status(500).json({ message: "Failed to retrieve user content" })
+    );
 });
-router.put('/users/:userId/publish', async (req, res) => {
-    try {
-        const userId = req.params.userId;
-        await Content.updateMany({ user: userId }, { isPublished: true });
-        res.status(200).json({ message: 'Content published successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error publishing content', error });
-    }
+router.put("/users/:userId/publish", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    await Content.updateMany({ user: userId }, { isPublished: true });
+    res.status(200).json({ message: "Content published successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error publishing content", error });
+  }
 });
 
 module.exports = router;
