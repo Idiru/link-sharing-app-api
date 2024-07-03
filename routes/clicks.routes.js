c5d940ca63a66badf86252c2594152256d20445e
Nawelchafi <chafinawel@gmail.com>
2024-07-03 23:40:15 +0200

    added fetching clicks by content id route

--- routes/clicks.routes.js
+++ routes/clicks.routes.js
@@ -18,5 +18,17 @@ router.post('/', async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 });
+router.get('/:contentId', async (req, res) => {
+    const { contentId } = req.params;
+
+    try {
+        const clicks = await Click.find({ content: contentId });
+        res.status(200).json(clicks);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
 
 module.exports = router;
